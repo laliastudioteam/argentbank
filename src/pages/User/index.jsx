2b7cbe286e0d05d459c98ca29ecc95669825512f
LaliaStudio <laliastudio@gmail.com>
2024-09-05 19:28:32 +0200

    Correctif  user

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {useEffect} from "react";
+// Import Redux
+import {useSelector} from "react-redux";
 // Import React-dom
 import {useParams, useNavigate} from "react-router-dom";
 
@@ -8,32 +10,42 @@ import Account from "../../components/Account";
 import Header from "../../components/Header";
 
 function User() {
-	return (
-		<main className="main bg-dark">
-			<Header />
-			<h2 className="sr-only">Accounts</h2>
-			<Account
-				data={{
-					title: "Argent Bank Checking (x8349)",
-					amount: "$2,082.79",
-					description: "Available Balance",
-				}}
-			/>
-			<Account
-				data={{
-					title: "Argent Bank Savings (x6712)",
-					amount: "$10,928.42",
-					description: "Available Balance",
-				}}
-			/>
-			<Account
-				data={{
-					title: "Argent Bank Credit Card (x8349)",
-					amount: "$184.30",
-					description: "Available Balance",
-				}}
-			/>
-		</main>
-	);
+	const navigate = useNavigate();
+	const token = useSelector(state => state.auth.token);
+
+	useEffect(() => {
+		if (!token) {
+			navigate("../home");
+		}
+	});
+	if (token) {
+		return (
+			<main className="main bg-dark">
+				<Header />
+				<h2 className="sr-only">Accounts</h2>
+				<Account
+					data={{
+						title: "Argent Bank Checking (x8349)",
+						amount: "$2,082.79",
+						description: "Available Balance",
+					}}
+				/>
+				<Account
+					data={{
+						title: "Argent Bank Savings (x6712)",
+						amount: "$10,928.42",
+						description: "Available Balance",
+					}}
+				/>
+				<Account
+					data={{
+						title: "Argent Bank Credit Card (x8349)",
+						amount: "$184.30",
+						description: "Available Balance",
+					}}
+				/>
+			</main>
+		);
+	}
 }
 export default User;
